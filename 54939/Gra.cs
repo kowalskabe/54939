47327bb42b3b9e93e663d9115f9e182c5a94dc85
kowalskabe <58075828+kowalskabe@users.noreply.github.com>
2020-06-14 11:41:19 +0200

    Add project files.

---
+++ 54939/Gra.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54939
+{
+    class Gra
+    {
+        internal protected string nazwa;
+        internal protected double cena;
+
+        public Gra() { }
+        public Gra (string nazwa, double cena)
+        {
+            this.nazwa = nazwa;
+            this.cena = cena;
+        }
+
+        public string Opis()
+        {
+            return nazwa;
+        }
+    }
+}
---
+++ 54939/GraPlanszowa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _54939
+{
+    class GraPlanszowa : Gra
+    {
+        private int liczbaGraczy;
+
+
+        public GraPlanszowa() { }
+
+        public GraPlanszowa(string nazwa, double cena, int liczbaGraczy) : base(nazwa, cena)
+        {
+            this.liczbaGraczy = liczbaGraczy;
+        }
+
+        
+        public double CzasRozgrywki
+    }
+}
